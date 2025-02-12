dc3822e6a5aaaaaa6b322f1b4dbec9c485f4c242
oliver-xp <oliver@matchlab.org>
2025-02-12 16:16:59 +0800

    fix: add nodePolyfills

--- react/vite/vite.config.ts
+++ react/vite/vite.config.ts
@@ -2,9 +2,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
+import {nodePolyfills} from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills({
+    protocolImports: true,
+  }),],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
