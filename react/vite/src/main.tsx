adf60c86ba03ca0d2b65e56cf311aff8e06f96db
oliver-xp <oliver@matchlab.org>
2025-02-25 17:17:30 +0800

    feat: add eruda

--- react/vite/src/main.tsx
+++ react/vite/src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import eruda from "eruda";
+eruda.init()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
